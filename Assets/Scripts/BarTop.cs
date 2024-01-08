66ba03f6b9944b16d5d74c1e20d1aa0ac41bd2bb
datlee240301 <devenewefall@gmail.com>
2024-01-08 17:27:52 +0700

    Revert "+ destroy chocolate to win"
    This reverts commit d33f79b3e719fd50cc0bebf2d30300e8d90da416.

--- Assets/Scripts/BarTop.cs
+++ Assets/Scripts/BarTop.cs
@@ -3,9 +3,9 @@
 public class BarTop : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         // Kiểm tra xem đối tượng va chạm có tag là "Choco" không
-        if (collision.CompareTag("Chocolate")) {
+        if (collision.CompareTag("Choco")) {
             // Lấy tất cả các đối tượng có tag "Choco" hiện tại trong Scene
-            //GameObject[] chocoObjects = GameObject.FindGameObjectsWithTag("Choco");
+            GameObject[] chocoObjects = GameObject.FindGameObjectsWithTag("Choco");
             EndGameManagerChocolate.instance.movedChoco++;
             EndGameManagerChocolate.instance.chocoAmonutToGet -=
                 EndGameManagerChocolate.instance.movedChoco;

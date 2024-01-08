1a4ce053feee1a9aa0e5d8fe3c81051f12f91284
datlee240301 <devenewefall@gmail.com>
2024-01-08 17:30:33 +0700

    +

--- Assets/Scripts/BarTop.cs
+++ Assets/Scripts/BarTop.cs
@@ -3,9 +3,7 @@
 public class BarTop : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         // Kiểm tra xem đối tượng va chạm có tag là "Choco" không
-        if (collision.CompareTag("Choco")) {
-            // Lấy tất cả các đối tượng có tag "Choco" hiện tại trong Scene
-            GameObject[] chocoObjects = GameObject.FindGameObjectsWithTag("Choco");
+        if (collision.CompareTag("Chocolate")) {
             EndGameManagerChocolate.instance.movedChoco++;
             EndGameManagerChocolate.instance.chocoAmonutToGet -=
                 EndGameManagerChocolate.instance.movedChoco;

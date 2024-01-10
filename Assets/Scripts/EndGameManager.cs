0c78db5ee88af8b30c78faeae19a0ac64457d21d
datlee240301 <devenewefall@gmail.com>
2024-01-10 15:31:54 +0700

    + create json file

--- Assets/Scripts/EndGameManager.cs
+++ Assets/Scripts/EndGameManager.cs
@@ -2,16 +2,19 @@
 using TMPro;
 using System.IO;
 
-public enum GameType {
+public enum GameType
+{
     Moves
 }
 
 [System.Serializable]
-public class EndGameRequirements {
+public class EndGameRequirements
+{
     public GameType gameType;
 }
 
-public class EndGameManager : MonoBehaviour {
+public class EndGameManager : MonoBehaviour
+{
     public static EndGameManager instance;
     public EndGameRequirements requirements;
     public TextMeshProUGUI counter;
@@ -26,19 +29,21 @@ public class EndGameManager : MonoBehaviour {
     public TextMeshProUGUI goalScoreText;
     public int goalScore;
     private LevelData currentLevelData; // Variable to hold the current level data
-    private string filePath = "Assets\\Resources";
+    //private string filePath = "Assets\\Resources";
 
-    private void Awake() {
-        if (instance == null) {
+    private void Awake()
+    {
+        if (instance == null)
+        {
             instance = this;
         }
         board = FindObjectOfType<Board>();
         LoadLevelData();
 
     }
-    void Start() {
+    void Start()
+    {
         SetUpGame();
-        
     }
 
     private void LoadLevelData()
@@ -49,32 +54,40 @@ public class EndGameManager : MonoBehaviour {
         currentCounterValue = currentLevelData._move;
     }
 
-    void SetUpGame() {
-        
-        if (requirements.gameType == GameType.Moves) {
+    void SetUpGame()
+    {
+
+        if (requirements.gameType == GameType.Moves)
+        {
             movesLabels.SetActive(true);
             //timesLabels.SetActive(false);
-        } else {
+        }
+        else
+        {
             timerSeconds = 1;
             movesLabels.SetActive(false);
-           // timesLabels.SetActive(true);
+            // timesLabels.SetActive(true);
         }
-        counter.text =  currentCounterValue.ToString();
+        counter.text = currentCounterValue.ToString();
     }
-    public void DecreaseCountervalue() {
+    public void DecreaseCountervalue()
+    {
         //if (board.currentState != GameState.pause) {
-            currentCounterValue--;
+        currentCounterValue--;
+        counter.text = "" + currentCounterValue;
+        if (currentCounterValue == 0)
+        {
+            board.currentState = GameState.pause;
+            currentCounterValue = 0;
             counter.text = "" + currentCounterValue;
-            if (currentCounterValue == 0) {
-                board.currentState = GameState.pause;
-                currentCounterValue = 0;
-                counter.text = "" + currentCounterValue;
             //}
         }
     }
 
-    public void SetEndGame() {
-        if (!setEndGame) {
+    public void SetEndGame()
+    {
+        if (!setEndGame)
+        {
             FindObjectOfType<PopupSetting>().PanelFadeIn();
             GameObjectLV1.Instance.LosePanelAppear();
             setEndGame = true;
@@ -83,8 +96,10 @@ public class EndGameManager : MonoBehaviour {
         }
     }
 
-    public void SetWinGame() {
-        if (!setWinGame) {
+    public void SetWinGame()
+    {
+        if (!setWinGame)
+        {
             FindObjectOfType<PopupSetting>().PanelFadeIn();
             GameObjectLV1.Instance.WinPanelAppear();
             setWinGame = true;
@@ -96,21 +111,26 @@ public class EndGameManager : MonoBehaviour {
 
     public void CreateJsonFile()
     {
+        string fileName = "vipro.json";
         string jsonData = JsonUtility.ToJson(FindObjectOfType<ScoreManager>().score);
+        string filePath = "Assets/Resources" + "/" + fileName;
         File.WriteAllText(filePath, jsonData);
         Debug.Log("Data saved to: " + filePath);
     }
 
-    void Update() {
-        if (currentCounterValue == 0) {
+    void Update()
+    {
+        if (currentCounterValue == 0)
+        {
             FindObjectOfType<Board>().currentState = GameState.pause;
             SetEndGame();
         }
-        if(FindObjectOfType<ScoreManager>().score >= goalScore) {
+        if (FindObjectOfType<ScoreManager>().score >= goalScore)
+        {
             FindObjectOfType<Board>().currentState = GameState.pause;
             SetWinGame();
         }
         goalScoreText.text = goalScore.ToString();
-        counter.text = currentCounterValue.ToString();  
+        counter.text = currentCounterValue.ToString();
     }
 }
